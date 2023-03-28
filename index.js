11b33c355119b2056116b461dde5bf4ca1112ff2
Orlando Umanzor <orlandojose729@gmail.com>
2023-03-27 21:26:02 -0600

    Refactor with Express.js

--- index.js
+++ index.js
@@ -1,28 +1,24 @@
-const http = require('http')
 const fs = require('fs/promises')
+const express = require('express')
+const path = require("path");
+const app = express()
 
-http.createServer(async (req, res) => {
-    try {
-        const parsedUrl = req.url.slice(1)
-        if(!parsedUrl){
-            const data = await fs.readFile(`index.html`)
-            res.writeHead(200, { 'Content-Type': 'text/html' })
-            return res.end(data)
-        } 
-        const data = await fs.readFile(`${parsedUrl}.html`)
-        res.writeHead(200, { 'Content-Type': 'text/html' })
-        return res.end(data)
-    } catch (error) {
-        console.log(error)
-        if(error.errno === -4058){
-            const data = await fs.readFile(`not-found.html`)
-            res.writeHead(200, { 'Content-Type': 'text/html' })
-            return res.end(data)
-        }
-        console.error("Error reading File", error)
-    }
-}).listen(8080, () => {
+
+app.use('/contact-me',express.static(__dirname, {index: 'contact-me.html'}))
+
+app.use('/', express.static(__dirname))
+
+app.use('/about', express.static(__dirname, { index: 'about.html' }))
+
+app.use((req, res) => {
+    res.status(404).sendFile(__dirname + '/not-found.html')
+})
+
+app.listen(8080, () => {
     console.log('Server running at http://localhost:8080/');
 })
+
+
+
 
 
